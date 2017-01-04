865dfeeb8fed6c196f3bbbd1b7e53f31251c5108
David Chun <daveedchun@gmail.com>
2017-01-04 12:22:30 -0800

    changed to carrierwave initializer to add endpoint

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@ CarrierWave.configure do |config|
     aws_secret_access_key: ENV['aws_secret_access_key'],                        # required
     # region:                'us-west-2',                  # optional, defaults to 'us-east-1'
     # host:                  's3.example.com',             # optional, defaults to nil
-    # endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
+    endpoint:              'https://s3-us-west-1.amazonaws.com/' # optional, defaults to nil
   }
   config.fog_directory  = 'twilio-mms'                          # required
   config.fog_public     = true                                        # optional, defaults to true
