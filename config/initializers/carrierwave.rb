6478c7828befa4b64c51d52ea3c5f5a6329050f6
David Chun <daveedchun@gmail.com>
2017-01-04 12:02:46 -0800

    set public to true

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,6 +10,6 @@ CarrierWave.configure do |config|
   }
   config.asset_host     = 'https://s3-us-west-1.amazonaws.com'
   config.fog_directory  = 'twilio-mms'                          # required
-  config.fog_public     = false                                        # optional, defaults to true
+  config.fog_public     = true                                        # optional, defaults to true
   config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
 end
