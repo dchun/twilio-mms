25529555891198530034c8311d22d46d27b1a8a9
David Chun <daveedchun@gmail.com>
2017-01-04 12:23:54 -0800

    changed to carrierwave initializer to add asset_host

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,9 +5,10 @@ CarrierWave.configure do |config|
     aws_secret_access_key: ENV['aws_secret_access_key'],                        # required
     # region:                'us-west-2',                  # optional, defaults to 'us-east-1'
     # host:                  's3.example.com',             # optional, defaults to nil
-    endpoint:              'https://s3-us-west-1.amazonaws.com/' # optional, defaults to nil
+    endpoint:              'https://s3-us-west-1.amazonaws.com' # optional, defaults to nil
   }
   config.fog_directory  = 'twilio-mms'                          # required
+  config.asset_host     = 'https://s3-us-west-1.amazonaws.com'                          # required
   config.fog_public     = true                                        # optional, defaults to true
   config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
 end
