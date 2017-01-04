692e7a6646e61a8d1f4a7ed4fe2cd653324be24b
David Chun <daveedchun@gmail.com>
2017-01-04 12:18:34 -0800

    changed to fog gem

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,12 @@
 CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     ENV['aws_access_key_id'],     # required
-    aws_secret_access_key: ENV['aws_secret_access_key'], # required
-    region:                'us-west-1',                  # optional, defaults to 'us-east-1'
-    host:                  's3-us-west-1.amazonaws.com',             # optional, defaults to nil
-    endpoint:              'https://s3-us-west-1.amazonaws.com' # optional, defaults to nil
+    aws_access_key_id:     ENV['aws_access_key_id'],                        # required
+    aws_secret_access_key: ENV['aws_secret_access_key'],                        # required
+    # region:                'us-west-2',                  # optional, defaults to 'us-east-1'
+    # host:                  's3.example.com',             # optional, defaults to nil
+    # endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
   }
-  config.asset_host     = 'https://s3-us-west-1.amazonaws.com'
   config.fog_directory  = 'twilio-mms'                          # required
   config.fog_public     = true                                        # optional, defaults to true
   config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
