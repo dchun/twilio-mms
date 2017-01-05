83c8f43956404ae0f55e294634c64a9b4a79068f
David Chun <daveedchun@gmail.com>
2017-01-05 10:39:14 -0800

    added scope to uniqueness validation in service model

--- app/models/service.rb
+++ app/models/service.rb
@@ -4,6 +4,6 @@ class Service < ApplicationRecord
   NAMES = ["twilio", "zoho"]
   validates_inclusion_of :name, in: NAMES, message: " should contain #{NAMES}."
   validates_presence_of :service_id, :authentication_token
-  validates_uniqueness_of :name
+  validates_uniqueness_of :name, scope: :user_id
 
 end
