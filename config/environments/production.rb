22a9d01f391faa2eff71e5e947f09a04823017c1
David Chun <daveedchun@gmail.com>
2017-01-10 10:49:24 -0800

    fix: forgot to add routes to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,3 +84,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+# for use with service model
+Rails.application.routes.default_url_options[:host] = 'https://twilio-mms.herokuapp.com'
